f504f234860f7b1f58bfb8b86c87b05f8eb7d2f2
xieziang <xza7914@163.com>
2023-03-20 23:56:43 +0800

    merge

--- SDK/cpp_project/macro.h
+++ SDK/cpp_project/macro.h
@@ -4,8 +4,8 @@
 const double INIT_PRIORITY = 2970.2978319553326;
 const double PRODUCT_ID = 100;
 const double LEVEL = 346.95775877784627;
-const double URGENCY = 1074.0858049658912;
-const double NINE_WORKSHOP = 1302.6704773921906;
-const double COLLIDE = 3000;
+const double URGENCY = 1100;
+const double NINE_WORKSHOP = 700;
+const double COLLIDE = 3700;
 
 #endif
